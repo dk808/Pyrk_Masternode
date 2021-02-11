34b82a9f96435cfd8e1c1d3beb3873d9c71efcd3
dk808 <41762330+dk808@users.noreply.github.com>
2021-02-10 16:24:40 -0800

    Add install.sh and update readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Pyrk_Masternode
+# Pyrk Masternode
+Script needs to be ran under a sudo user and not under root. It will install latest binaries, configure basic firewall settings(optional), install sentinel(optional), and create a daemon service for you. It also has a bootstrap option for quick syncing.
+
+> ℹ Note: This has only been tested on Ubuntu 18. USE AT OWN RISK.
+
+## Installation
+Create a sudo user and run this under that sudo user. Script will exit if ran under root.  
+Script will ask for BLS Secret Key(operatorSecret) that you get from the bls generate command. So have it ready.
+```bash
+bash <(curl -s https://raw.githubusercontent.com/dk808/Pyrk_Masternode/main/install.sh)
+```
+> ℹ Info: This will also create a script to update binaries.  
+If you need any support please join their [Discord](https://discord.gg/4FrYVZJpkD).
---
+++ install.sh
@@ -0,0 +1,369 @@
+#!/bin/bash
+
+COIN_NAME='pyrk'
+
+#wallet information
+BOOTSTRAP_ZIP='https://www.dropbox.com/s/8e9i40gwx5oow0x/pyrk-bootstrap.zip'
+BOOTSTRAP_ZIPFILE='pyrk-bootstrap.zip'
+CONFIG_DIR='.pyrk'
+CONFIG_FILE='pyrk.conf'
+PORT='8118'
+SSHPORT='22'
+COIN_DAEMON='pyrkd'
+COIN_CLI='pyrk-cli'
+COIN_PATH='/usr/local/bin'
+USERNAME="$(whoami)"
+
+#color codes
+RED='\033[1;31m'
+YELLOW='\033[1;33m'
+BLUE="\\033[38;5;27m"
+SEA="\\033[38;5;49m"
+GREEN='\033[1;32m'
+CYAN='\033[1;36m'
+BLINKRED='\033[1;31;5m'
+NC='\033[0m'
+STOP='\e[0m'
+
+#emoji codes
+X_POINT="${BLINKRED}\xE2\x9D\x97${NC}"
+
+#end of required details
+#
+
+echo -e "${YELLOW}==========================================================="
+echo -e 'PYRK Masternode Setup'
+echo -e "===========================================================${NC}"
+echo -e "${BLUE}Feb 2021, created by dk808${NC}"
+echo -e
+echo -e "${CYAN}Node setup starting, press [CTRL-C] to cancel.${NC}"
+sleep 5
+if [ "$USERNAME" = "root" ]; then
+    echo -e "${CYAN}You are currently logged in as ${NC}root${CYAN}, please switch to a sudo user.${NC}"
+    exit
+fi
+
+#functions
+function wipe_clean() {
+    echo -e "${YELLOW}Removing any instances of Pyrk...${NC}"
+    sudo systemctl stop $COIN_NAME > /dev/null 2>&1 && sleep 2
+    sudo $COIN_CLI stop > /dev/null 2>&1 && sleep 2
+    sudo killall $COIN_DAEMON > /dev/null 2>&1
+    sudo rm /usr/local/bin/$COIN_NAME* > /dev/null 2>&1 && sleep 1
+    sudo rm /usr/bin/$COIN_NAME* > /dev/null 2>&1 && sleep 1
+    rm update.sh
+    rm -rf $BOOTSTRAP_ZIPFILE
+    rm -rf sentinel
+}
+
+function ssh_port() {
+    echo -e "${YELLOW}Detecting SSH port being used...${NC}" && sleep 1
+    SSHPORT=$(grep -w Port /etc/ssh/sshd_config | sed -e 's/.*Port //')
+    if ! whiptail --yesno "Detected you are using $SSHPORT for SSH is this correct?" 8 56; then
+        SSHPORT=$(whiptail --inputbox "Please enter port you are using for SSH" 8 43 3>&1 1>&2 2>&3)
+        echo -e "${YELLOW}Using SSH port:${SEA} $SSHPORT${NC}" && sleep 1
+    else
+        echo -e "${YELLOW}Using SSH port:${SEA} $SSHPORT${NC}" && sleep 1
+    fi
+}
+
+function ip_confirm() {
+    echo -e "${YELLOW}Detecting IP address being used...${NC}" && sleep 1
+    WANIP=$(wget http://ipecho.net/plain -O - -q)
+    if ! whiptail --yesno "Detected IP address is $WANIP is this correct?" 8 60; then
+        WANIP=$(whiptail --inputbox "        Enter IP address" 8 36 3>&1 1>&2 2>&3)
+    fi
+}
+
+function create_swap() {
+    echo -e "${YELLOW}Creating swap if none detected...${NC}" && sleep 1
+    if ! grep -q "swapfile" /etc/fstab; then
+        if whiptail --yesno "No swapfile detected would you like to create one?" 8 54; then
+          sudo fallocate -l 4G /swapfile
+          sudo chmod 600 /swapfile
+          sudo mkswap /swapfile
+          sudo swapon /swapfile
+          echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+          echo -e "${YELLOW}Created ${SEA}4G${YELLOW} swapfile${NC}"
+        fi
+    fi
+    sleep 2
+}
+
+function install_packages() { 
+    echo -e "${YELLOW}Installing Packages...${NC}"
+    sudo apt-get update -y
+    sudo apt-get upgrade -y
+    sudo apt-get install -y \
+        git \
+        nano \
+        htop \
+        pwgen \
+        figlet \
+        unzip \
+        jq \
+        python3 \
+        virtualenv
+    echo -e "${YELLOW}Packages complete...${NC}"
+}
+
+function spinning_timer() {
+    animation=( ⠋ ⠙ ⠹ ⠸ ⠼ ⠴ ⠦ ⠧ ⠇ ⠏ )
+    end=$((SECONDS+NUM))
+    while [ $SECONDS -lt $end ];
+    do
+        for i in "${animation[@]}";
+        do
+            echo -ne "${RED}\r$i ${CYAN}${MSG1}${NC}"
+            sleep 0.1
+        done
+    done
+    echo -e "${MSG2}"
+}
+
+function create_conf() {
+    if [ -f $HOME/$CONFIG_DIR/$CONFIG_FILE ]; then
+        echo -e "${CYAN}Existing conf file found backing up to $COIN_NAME.old ...${NC}"
+        mv $HOME/$CONFIG_DIR/$CONFIG_FILE $HOME/$CONFIG_DIR/$COIN_NAME.old;
+    fi
+    RPCUSER=$(pwgen -1 8 -n)
+    PASSWORD=$(pwgen -1 20 -n)
+    masternodeblsprivkey=$(whiptail --inputbox "Enter your Masternode BLS secret key" 8 75 3>&1 1>&2 2>&3)
+    echo -e "${YELLOW}Creating Conf File...${NC}"
+    sleep 1
+    mkdir $HOME/$CONFIG_DIR > /dev/null 2>&1
+    touch $HOME/$CONFIG_DIR/$CONFIG_FILE
+    cat << EOF > $HOME/$CONFIG_DIR/$CONFIG_FILE
+rpcuser=$RPCUSER
+rpcpassword=$PASSWORD
+rpcallowip=127.0.0.1
+rpcport=8119
+port=$PORT
+server=1
+daemon=1
+listen=1
+masternodeblsprivkey=$masternodeblsprivkey
+externalip=$WANIP
+addnode=167.86.79.133
+addnode=145.209.168.81
+addnode=167.86.91.64
+addnode=173.212.223.176
+addnode=103.244.37.78
+addnode=207.180.208.104
+maxconnections=256
+EOF
+}
+
+function install_bins() {
+    echo -e "${YELLOW}Installing latest binaries...${NC}"
+    WALLET_TAR=$(curl -s https://api.github.com/repos/pyrkcommunity/pyrk/releases/latest | jq -r '.assets[] | select(.name|test("linux64.")) | .browser_download_url')
+    mkdir temp
+    curl -L $WALLET_TAR | tar xz -C ./temp; sudo mv ./temp/$COIN_DAEMON ./temp/$COIN_CLI ./temp/$COIN_NAME-tx $COIN_PATH
+    sudo chmod 755 ${COIN_PATH}/${COIN_NAME}*
+    rm -rf temp
+}
+
+function bootstrap() {
+    if whiptail --yesno "Would you like to bootstrap the chain?" 8 42; then
+        if [ -d $HOME/$CONFIG_DIR/blocks ]; then
+            rm -rf $HOME/$CONFIG_DIR/blocks $HOME/$CONFIG_DIR/chainstate $HOME/$CONFIG_DIR/smsgDB $HOME/$CONFIG_DIR/evodb
+            echo -e "${YELLOW}Downloading wallet bootstrap please be patient...${NC}"
+            wget $BOOTSTRAP_ZIP
+            unzip $BOOTSTRAP_ZIPFILE -d $HOME/$CONFIG_DIR
+            rm -rf $BOOTSTRAP_ZIPFILE
+        else
+            echo -e "${YELLOW}Downloading wallet bootstrap please be patient...${NC}"
+            wget $BOOTSTRAP_ZIP
+            unzip $BOOTSTRAP_ZIPFILE -d $HOME/$CONFIG_DIR
+            rm -rf $BOOTSTRAP_ZIPFILE
+        fi
+    else
+        echo "${YELLOW}Skipping bootstrap...${NC}"
+    fi
+}
+
+function update_script() {
+    echo -e "${YELLOW}Creating a script to update binaries for future updates...${NC}"
+    touch $HOME/update.sh
+    cat << EOF > $HOME/update.sh
+#!/bin/bash
+WALLET_TAR=\$(curl -s https://api.github.com/repos/pyrkcommunity/pyrk/releases/latest | jq -r '.assets[] | select(.name|test("linux64.")) | .browser_download_url')
+COIN_NAME='pyrk'
+COIN_DAEMON='pyrkd'
+COIN_CLI='pyrk-cli'
+COIN_PATH='/usr/local/bin'
+sudo systemctl stop \$COIN_NAME
+\$COIN_CLI stop > /dev/null 2>&1 && sleep 2
+sudo killall \$COIN_DAEMON > /dev/null 2>&1
+sudo rm /usr/local/bin/\$COIN_NAME* > /dev/null 2>&1
+mkdir temp
+curl -L \$WALLET_TAR | tar xz -C ./temp; sudo mv ./temp/\$COIN_DAEMON ./temp/\$COIN_CLI ./temp/\$COIN_NAME-tx \$COIN_PATH
+rm -rf temp
+sudo chmod 755 \${COIN_PATH}/\${COIN_NAME}*
+sudo systemctl start \$COIN_NAME > /dev/null 2>&1
+EOF
+    sudo chmod +x update.sh
+}
+
+function install_sentinel() {
+    if whiptail --yesno "Would you like to install sentinel?" 8 39; then
+        echo -e "${YELLOW}Installing sentinel...${NC}"
+        git clone https://github.com/pyrkcommunity/sentinel.git && cd sentinel
+        virtualenv -p $(which python3) ./venv
+        ./venv/bin/pip install -r requirements.txt
+        #sentinel conf
+        SENTINEL_CONF=$(cat <<EOF
+pyrk_conf=$HOME/$CONFIG_DIR/$CONFIG_FILE
+db_name=database/sentinel.db
+db_driver=sqlite
+network=mainnet
+EOF
+)
+        echo "$SENTINEL_CONF" > $HOME/sentinel/sentinel.conf
+        cd
+        crontab -l | grep -v "$HOME/sentinel && SENTINEL_DEBUG=1 ./venv/bin/python bin/sentinel.py" | crontab -
+        sleep 1
+        crontab -l > tempcron
+        echo "* * * * * cd $HOME/sentinel && SENTINEL_DEBUG=1 ./venv/bin/python bin/sentinel.py >> sentinel.log 2>&1" >> tempcron
+        crontab tempcron
+        rm tempcron
+    fi
+}
+
+function create_service() {
+    echo -e "${YELLOW}Creating Pyrk service...${NC}"
+    sudo touch /etc/systemd/system/$COIN_NAME.service
+    sudo chown $USERNAME:$USERNAME /etc/systemd/system/$COIN_NAME.service
+    cat << EOF > /etc/systemd/system/$COIN_NAME.service
+[Unit]
+Description=$COIN_NAME service
+After=network.target
+[Service]
+Type=forking
+User=$USERNAME
+Group=$USERNAME
+WorkingDirectory=$HOME/$CONFIG_DIR/
+ExecStart=$COIN_PATH/$COIN_DAEMON -datadir=$HOME/$CONFIG_DIR/ -conf=$HOME/$CONFIG_DIR/$CONFIG_FILE -daemon
+ExecStop=-$COIN_PATH/$COIN_CLI stop
+Restart=always
+RestartSec=3
+PrivateTmp=true
+TimeoutStopSec=60s
+TimeoutStartSec=10s
+StartLimitInterval=120s
+StartLimitBurst=5
+[Install]
+WantedBy=multi-user.target
+EOF
+    sudo chown root:root /etc/systemd/system/$COIN_NAME.service
+    sudo systemctl daemon-reload
+    sleep 4
+    sudo systemctl enable $COIN_NAME > /dev/null 2>&1
+}
+
+function basic_security() {
+    if whiptail --yesno "Would you like to setup basic firewall and fail2ban?" 8 56; then
+        echo -e "${YELLOW}Configuring firewall and enabling fail2ban...${NC}"
+        sudo apt-get install ufw fail2ban -y
+        sudo ufw allow $SSHPORT/tcp
+        sudo ufw allow $PORT/tcp
+        sudo ufw logging on
+        sudo ufw default deny incoming
+        sudo ufw default allow outgoing
+        sudo ufw limit OpenSSH
+        echo "y" | sudo ufw enable > /dev/null 2>&1
+        sudo touch /etc/fail2ban/jail.local
+        sudo chown $USERNAME:$USERNAME /etc/fail2ban/jail.local
+        cat << EOF > /etc/fail2ban/jail.local
+[sshd]
+enabled = true
+port = $SSHPORT
+filter = sshd
+logpath = /var/log/auth.log
+maxretry = 3
+EOF
+        sudo chown root:root /etc/fail2ban/jail.local
+        sudo systemctl restart fail2ban > /dev/null 2>&1
+        sudo systemctl enable fail2ban > /dev/null 2>&1
+    else
+        echo -e "${YELLOW}Skipping basic security...${NC}"
+    fi
+}
+
+function start_daemon() {
+    NUM='180'
+    MSG1='Starting daemon service & syncing chain please be patient this will take few min...'
+    MSG=''
+    if sudo systemctl start $COIN_NAME > /dev/null 2>&1; then
+        echo && spinning_timer
+        NUM='5'
+        MSG1='Getting blockchain info...'
+        MSG2=''
+        echo && spinning_timer
+        echo
+        $COIN_CLI getinfo
+    else
+        echo -e "${RED}Something is not right the daemon did not start. Will exit out so try and run the script again.${NC}"
+        exit
+    fi
+}
+
+function log_rotate() {
+    echo -e "${YELLOW}Configuring logrotate function for debug log...${NC}"
+    sleep 1
+    if [ -f /etc/logrotate.d/pyrkdebuglog ]; then
+        echo -e "${YELLOW}Existing log rotate conf found, backing up to ~/pyrkdebuglogrotate.old ...${NC}"
+        sudo mv /etc/logrotate.d/pyrkdebuglog ~/pyrkdebuglogrotate.old
+        sleep 2
+    fi
+    sudo touch /etc/logrotate.d/pyrkdebuglog
+    sudo chown $USERNAME:$USERNAME /etc/logrotate.d/pyrkdebuglog
+    cat << EOF > /etc/logrotate.d/pyrkdebuglog
+$HOME/.pyrk/debug.log {
+  compress
+  copytruncate
+  missingok
+  daily
+  rotate 7
+}
+EOF
+    sudo chown root:root /etc/logrotate.d/pyrkdebuglog
+}
+
+#
+#end of functions
+
+#run functions
+  wipe_clean
+  ssh_port
+  ip_confirm
+  create_swap
+  install_packages
+  create_conf
+  install_bins
+  bootstrap
+  install_sentinel
+  create_service
+  basic_security
+  start_daemon
+  log_rotate
+  update_script
+  
+printf "${BLUE}"
+figlet -t -k "PYRK  MASTERNODES" 
+printf "${STOP}"
+
+echo -e "${YELLOW}==========================================================================================================="
+echo -e "${CYAN}COURTESY OF DK808 FROM ALTTANK${NC}"
+echo
+echo -e "${YELLOW}Commands to manage $COIN_NAME service${NC}"
+echo -e "  TO START- ${CYAN}sudo systemctl start $COIN_NAME${NC}"
+echo -e "  TO STOP - ${CYAN}sudo systemctl stop $COIN_NAME${NC}"
+echo -e "  STATUS  - ${CYAN}sudo systemctl status $COIN_NAME${NC}"
+echo -e "In the event server ${RED}reboots${NC} daemon service will ${GREEN}auto-start${NC}"
+echo
+echo -e "${X_POINT}${X_POINT} ${YELLOW}To use $COIN_CLI simply start command with $COIN_CLI" ${X_POINT}${X_POINT}
+echo -e "     ${YELLOW}E.g ${CYAN}$COIN_CLI getblockchaininfo${NC}"
+echo
+echo -e "${YELLOW}To update binaries when new ones are released enter ${SEA}./update.sh${NC}"
+echo -e "${YELLOW}===========================================================================================================${NC}"
